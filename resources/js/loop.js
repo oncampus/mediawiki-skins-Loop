e035bdd4d80c0094e799faf59027bff64d194d1e
Krohn <krohnd@th-luebeck.de>
2019-04-08 13:02:10 +0200

    plyr changes

--- resources/js/loop.js
+++ resources/js/loop.js
@@ -56,7 +56,27 @@ $( document ).ready( function () {
 			const player = new Plyr("#t2s-audio", {
 				"volume": 1,
 				"autoplay": true,
-				"muted": false
+				"muted": false,
+				"iconUrl": mw.config.get("stylepath") + "/Loop/node_modules/plyr/dist/plyr.svg", // use svg icons from server, not from cdn
+				"controls": [
+					'play', // Play/pause playback
+					'progress', // The progress bar and scrubber for playback and buffering
+					'current-time', // The current time of playback
+					'mute', // Toggle mute
+					'captions', // Toggle captions
+					'settings', // Settings menu
+					/* optional settings to add */
+					//'play-large', // The large play button in the center
+					//'restart', // Restart playback
+					//'rewind', // Rewind by the seek time (default 10 seconds)
+					//'fast-forward', // Fast forward by the seek time (default 10 seconds)
+					//'duration', // The full duration of the media
+					//'volume', // Volume control
+					//'pip', // Picture-in-picture (currently Safari only)
+					//'airplay', // Airplay (currently Safari only)
+					//'download', // Show a download button with a link to either the current source or a custom URL you specify in your options
+					//'fullscreen', // Toggle fullscreen
+				]
 			});
 			
 		}).fail(function(xhr, textStatus, errorThrown) { 
