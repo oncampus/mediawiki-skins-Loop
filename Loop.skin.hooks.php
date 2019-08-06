2cfe2bc1a67bc1f7c050b1e9ba9074f15018f342
krohnden <dennis.krohn@fh-luebeck.de>
2019-08-06 16:32:10 +0200

    extended links support for html

--- Loop.skin.hooks.php
+++ Loop.skin.hooks.php
@@ -51,30 +51,51 @@ class LoopSkinHooks {
 
 			$loopHtml = new LoopHtml();
 
-			if ( isset( $target->mArticleID ) ) { # don't pick special pages
+			if ( isset( $target->mArticleID ) ) { # don't pick special page links
 				$targetTitle = $target->mTextform ;
 				$lsi = LoopStructureItem::newFromText( $targetTitle );
-
-				if ( $lsi ) { # internal pages in structure
+				if ( $lsi ) { # links to internal pages in structure
 					$newHref = $loopHtml->resolveUrl( $target->mUrlform, '.html');
 					$attribs['href'] = $newHref;
 					$attribs["class"] = $attribs["class"] . " local-link";
 
-				} elseif ( isset ($attribs['id'] ) ) {
+				} elseif ( isset ($attribs['id'] ) ) { # privacy and imprint links
 					if ( $attribs['id'] == "imprintlink" || $attribs['id'] == "privacylink" ) { 
 						$newHref = $loopHtml->resolveUrl( $target->mUrlform, '.html');
 						$attribs['href'] = $newHref;
 						$attribs["class"] = $attribs["class"] . " local-link";
 					}
+				} elseif( $target->getNamespace() == NS_GLOSSARY ) { # pages in glossary
+				    $newHref = $loopHtml->resolveUrl( $target->prefixedText, '.html');
+				    $attribs['href'] = $newHref;
+				    $attribs["class"] = $attribs["class"] . " local-link";
 				} else { # internal pages that are not in structure
 					$attribs['href'] = $wgServer . $attribs['href'];
 				}
 
 			} elseif ( isset ($attribs['id']) && $attribs['id'] == "toc-button" ) { # TOC button link
-				$newHref = $loopHtml->resolveUrl( "LoopStructure", '.html'); 
+			    $newHref = wfMessage( "loopstructure" )->text(). '.html'; 
 				$attribs['href'] = $newHref;
 				$attribs["class"] = $attribs["class"] . " local-link";
-			} 
+			} elseif ( !empty ( $attribs["class"] ) ) {
+			    $newHref = '#';
+			    if ( strpos( "aToc", $attribs["class"] ) !== false ) { # toc sidebar links
+			        $pageTitle = $attribs["id"];
+			        $newHref = wfMessage( strtolower($pageTitle) )->text() . '.html';
+			    } elseif ( strpos( "literature-link", $attribs["class"] ) !== false ) { # literature links in content
+			        $newHref =  wfMessage( "loopliterature" )->text() . '.html';
+			        if ( isset( $attribs["data-target"])) {
+			            $newHref .= "#". $attribs["data-target"];
+			        }
+			    }
+			    $attribs['href'] = $newHref;
+			    $attribs["class"] .= " local-link";
+			}
+		} else {
+		    # Add id to loop_reference links
+		    if ( isset( $attribs["data-target"])) {
+		        $attribs["href"] .= "#". $attribs["data-target"];
+		    }
 		}
 
 		return true;
