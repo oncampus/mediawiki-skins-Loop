248d4a602d0d52d7b485aac237544b2e47ce3c93
oncampus <education@oncampus.de>
2020-07-28 15:48:37 +0200

    fixed various issues

--- Loop.skin.hooks.php
+++ Loop.skin.hooks.php
@@ -1,38 +1,38 @@
-<?php 
+<?php
 
 use MediaWiki\MediaWikiServices;
 
 class LoopSkinHooks {
-	
+
 	/**
 	 * Show edit links only in LoopEditMode
 	 *
      * This is attached to the MediaWiki 'SkinEditSectionLinks' hook.
 	 *
-	 * @param Skin $skin 
-	 * @param Title $title 
-	 * @param string $section 
-	 * @param string $tooltip 
-     * @param array $result 
-	 * @param Language $lang 
+	 * @param Skin $skin
+	 * @param Title $title
+	 * @param string $section
+	 * @param string $tooltip
+     * @param array $result
+	 * @param Language $lang
 	 * @return bool true
-	 */	
+	 */
 	public static function onSkinEditSectionLinks( Skin $skin, Title $title, $section, $tooltip, &$result, $lang ) {
-		
-		global $wgDefaultUserOptions; 
-		
+
+		global $wgDefaultUserOptions;
+
 		$loopeditmode = $skin->getUser()->getOption( 'LoopEditMode', false, true );
 		$looprendermode = $skin->getUser()->getOption( 'LoopRenderMode', $wgDefaultUserOptions['LoopRenderMode'], true );
-		
+
 		if ( $loopeditmode && $looprendermode == "default" ) {
 			$result[ 'editsection' ][ 'text' ] = new HtmlArmor('<span class="ic ic-edit"></span>');
 		} else {
 			$result[ 'editsection' ][ 'text' ] = '';
 		}
-		
+
 		return true;
 	}
-	
+
 	/**
 	 * Change internal links in offline mode to local paths
 	 *
@@ -49,7 +49,7 @@ class LoopSkinHooks {
 		global $wgOut, $wgCanonicalServer;
 
 		$looprendermode = $wgOut->getUser()->getOption( 'LoopRenderMode', false, true );
-		
+
 		if ( $looprendermode == "offline" ) {
 			$loopHtml = new LoopHtml();
 
@@ -70,7 +70,7 @@ class LoopSkinHooks {
 				}
 
 			} elseif ( isset ($attribs['id']) && $attribs['id'] == "toc-button" ) { # TOC button link
-				$newHref = wfMessage( "loopstructure" )->text(). '.html'; 
+				$newHref = wfMessage( "loopstructure" )->text(). '.html';
 				$attribs['href'] = $newHref;
 				$attribs["class"] = $attribs["class"] . " local-link";
 			} elseif ( !empty ( $attribs["class"] ) ) {
@@ -88,12 +88,12 @@ class LoopSkinHooks {
 				$attribs["class"] .= " local-link";
 			}
 			if ( isset ($attribs['id'] ) ) {
-				if ( $attribs['id'] == "imprintlink" || $attribs['id'] == "privacylink" ) { 
+				if ( $attribs['id'] == "imprintlink" || $attribs['id'] == "privacylink" ) {
 					$attribs["class"] = $attribs["class"] . "local-link";
-				} 
-			} 
+				}
+			}
 		}
-		
+
 		# Add id to loop_reference links
 		if ( isset( $attribs["data-target"])) {
 			$attribs["href"] .= "#". $attribs["data-target"];
@@ -119,36 +119,42 @@ class LoopSkinHooks {
 	public static function onMediaWikiPerformAction( $output, $article, $title, $user, $request, $wiki ) {
 
 		global $wgLoopUrlSkinStyles;
-	
+
 		$loopSkinStyleRequestValue = 'style-' . $request->getText( 'skin' );
-		
+
 		if( in_array( $loopSkinStyleRequestValue, $wgLoopUrlSkinStyles ) ) {
 			$user->setOption( 'LoopSkinStyle', $loopSkinStyleRequestValue );
 			$user->saveSettings();
 		}
-	
+
 		return true;
-	}	
-	
+	}
+
 	public static function onBeforePageDisplay( OutputPage $out, Skin $skin ) {
 		$linkRenderer = MediaWikiServices::getInstance()->getLinkRenderer();
 		$currentId = $out->getTitle()->getArticleID();
 		$lsItem = LoopStructureItem::newFromIds( $currentId );
-		
+
 		if(is_object($lsItem)) {
 			if( $lsItem->previousArticle !== '0' ) {
-				$link = $linkRenderer->makeLink( Title::newFromId( $lsItem->previousArticle ) );
-				$xml = simplexml_load_string( $link );
-				$list = $xml->xpath( "//@href" );
-				$prevArticle = parse_url( $list[0])['path'];
-				$out->addJsConfigVars( 'jsSwipePrev', $prevArticle );
+				$title = Title::newFromId( $lsItem->previousArticle );
+				if( is_object($title) ) {
+					$link = $linkRenderer->makeLink( $title );
+					$xml = simplexml_load_string( $link );
+					$list = $xml->xpath( "//@href" );
+					$prevArticle = parse_url( $list[0])['path'];
+					$out->addJsConfigVars( 'jsSwipePrev', $prevArticle );
+				}
 			}
 			if( $lsItem->nextArticle !== '0') {
-				$link = $linkRenderer->makeLink( Title::newFromId( $lsItem->nextArticle ) );
-				$xml = simplexml_load_string( $link );
-				$list = $xml->xpath( "//@href" );
-				$nextArticle = parse_url( $list[0] )['path'];
-				$out->addJsConfigVars( 'jsSwipeNext', $nextArticle );
+				$title = Title::newFromId( $lsItem->nextArticle );
+				if( is_object($title) ) {
+					$link = $linkRenderer->makeLink( $title );
+					$xml = simplexml_load_string( $link );
+					$list = $xml->xpath( "//@href" );
+					$nextArticle = parse_url( $list[0] )['path'];
+					$out->addJsConfigVars( 'jsSwipeNext', $nextArticle );
+				}
 			}
 		}
 	}
